8cc6c3d6c31049da4e6a96f54a3b3415342750fc
Juan Sebastian Guauque Torres <juansebastian.guauque@gmail.com>
2022-07-07 12:57:12 -0500

    Initial commit

---
+++ lib/guauque_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "guauque_view_tool/version"
+require_relative "guauque_view_tool/renderer"
+
+module GuauqueViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
